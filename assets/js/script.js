08953d82a0d6da8b8613c7d2a2196bd08379cd5b
Iryna Walsh <i2pi8bl7@students.codeinstitute.net>
2024-09-14 21:16:39 +0000

    Create:scrip structure for all the future functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,53 @@
-function startQuiz () {
+// --- Start and Display the Quiz ---//
+function startQuiz() {
 
 }
 
+//--- Display Painting/Question Number, after 11th Image, go to Quiz End ---//
+function imageNumber() {
 
+}
+
+//-- Display Image of the Painting --//
+function getImage() {
+
+}
+
+//-- Shuffle through the questions/images arrays --//
+function shuffleQuestions() {
+
+}
+
+//-- Display answer options --//
+function getAnswerOptions() {
+
+}
+
+//-- Handle answer selection --//
+function chooseAnswer() {
+
+}
+
+//-- Check correct answer and display the correct one if wrong and activate the facts text --//
+function checkRightAnswer() {
+
+}
+
+//-- Generate next question when button pressed --//
+function getNextQuestion() {
+
+}
+
+//--- Add to the score if answer correct ---//
+function addScore() {
+
+}
+
+//--- Final End of Quiz Message with a score and play again option ---//
+function endQuiz() {
+
+}
+
+function playAgain() {
+
+}
