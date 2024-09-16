9a130c4954e27cbf1cfd9ea6c19d424c26fbdda0
Iryna Walsh <i2pi8bl7@students.codeinstitute.net>
2024-09-16 12:09:32 +0000

    Create: arrays of quiz questions content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,27 @@
 
 //-- Create variables and store elements there --//
-    const startBtn = document.getElementById("start");
-    const introArea = document.getElementById("intro");
-    const questionsArea = document.getElementById("questions");
-    const scoreBox = document.getElementById("score-box");
-    const optionOneBtn = document.getElementById("option-one");
-    const optionTwoBtn = document.getElementById("option-two");
-    const optionThreeBtn = document.getElementById("option-three");
-    const optionFourBtn = document.getElementById("option-four");
-    const nextQuestionBtn = document.getElementById("next-question");
-    const factsArea = document.getElementById("art-facts");
-    const quizEndArea = document.getElementById("quiz-end");
-    const scoreMessage = document.getElementById("score-message");
-    const tryAgainBtn = document.getElementById("try-again");
+const startBtn = document.getElementById("start");
+const introArea = document.getElementById("intro");
+const questionsArea = document.getElementById("questions");
+const scoreBox = document.getElementById("score-box");
+const optionOneBtn = document.getElementById("option-one");
+const optionTwoBtn = document.getElementById("option-two");
+const optionThreeBtn = document.getElementById("option-three");
+const optionFourBtn = document.getElementById("option-four");
+const nextQuestionBtn = document.getElementById("next-question");
+const factsArea = document.getElementById("art-facts");
+const quizEndArea = document.getElementById("quiz-end");
+const scoreMessage = document.getElementById("score-message");
+const tryAgainBtn = document.getElementById("try-again");
 
-    // Start the Quiz event listener
-    startBtn.addEventListener("click", startQuiz);
+// Start the Quiz event listener
+startBtn.addEventListener("click", startQuiz);
+
+//current question numer sequence
+let currentNumber = 0;
+// the users points score
+let score = 0;
+
 
 //--- Create Quiz functions to run the game ---//
 // --- Start and Display the Quiz ---//
@@ -23,6 +29,14 @@ function startQuiz() {
     introArea.classList.add("hidden");
     questionsArea.classList.remove("hidden");
     scoreBox.classList.remove("hidden-two")
+    imageNumber();
+    getImage();
+    optionOneBtn.addEventListener("click", checkAnswer);
+    optionTwoBtn.addEventListener("click", checkAnswer);
+    optionThreeBtn.addEventListener("click", checkAnswer);
+    optionFourBtn.addEventListener("click", checkAnswer);
+    shuffleQuestions();
+    getFacts();
 }
 
 //--- Display Painting/Question Number, after 11th Image, go to Quiz End ---//
@@ -45,13 +59,12 @@ function getAnswerOptions() {
 
 }
 
-//-- Handle answer selection --//
-function chooseAnswer() {
+//-- Check correct answer and display the correct one if wrong and activate the facts text --//
+function checkAnswer() {
 
 }
 
-//-- Check correct answer and display the correct one if wrong and activate the facts text --//
-function checkRightAnswer() {
+function getFacts() {
 
 }
 
@@ -73,3 +86,95 @@ function endQuiz() {
 function playAgain() {
 
 }
+
+// --- Arrays --- //
+const questionContent = [
+    {
+        number: 1,
+        image: "assets/images/alphonse-mucha.jpg",
+        answers: ["Gustav Klimt", "Aubrey Beardsley", "Alphonse Mucha", "Egon Schiele"],
+        facts: "'Fruit' by Alphonse Mucha. he was a Czech painter, illustrator, and graphic artist. Living in Paris during the Art Nouveau period, he was widely known for his distinctly stylized and decorative theatrical posters",
+        correctAnswer: "Alphonse Mucha"
+    },
+    {
+        number: 2,
+        image: "assets/images/andy-warhal.jpg",
+        answers: ["", "", "", ""],
+        facts: "",
+        correctAnswer: ""
+
+    },
+    {
+        number: 3,
+        image: "assets/images/andy-warhal.jpg",
+        answers: ["", "", "", ""],
+        facts: "",
+        correctAnswer: ""
+
+    },
+    {
+        number: 4,
+        image: "assets/images/andy-warhal.jpg",
+        answers: ["", "", "", ""],
+        facts: "",
+        correctAnswer: ""
+
+    },
+    {
+        number: 5,
+        image: "assets/images/andy-warhal.jpg",
+        answers: ["", "", "", ""],
+        facts: "",
+        correctAnswer: ""
+
+    },
+    {
+        number: 6,
+        image: "assets/images/andy-warhal.jpg",
+        answers: ["", "", "", ""],
+        facts: "",
+        correctAnswer: ""
+
+    },
+    {
+        number: 7,
+        image: "assets/images/andy-warhal.jpg",
+        answers: ["", "", "", ""],
+        facts: "",
+        correctAnswer: ""
+
+    },
+    {
+        number: 8,
+        image: "assets/images/andy-warhal.jpg",
+        answers: ["", "", "", ""],
+        facts: "",
+        correctAnswer: ""
+
+    },
+    {
+        number: 9,
+        image: "assets/images/andy-warhal.jpg",
+        answers: ["", "", "", ""],
+        facts: "",
+        correctAnswer: ""
+
+    },
+    {
+        number: 10,
+        image: "assets/images/andy-warhal.jpg",
+        answers: ["", "", "", ""],
+        facts: "",
+        correctAnswer: ""
+
+    },
+    {
+        number: 11,
+        image: "assets/images/andy-warhal.jpg",
+        answers: ["", "", "", ""],
+        facts: "",
+        correctAnswer: ""
+
+    }
+
+]
