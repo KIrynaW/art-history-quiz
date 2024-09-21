b88b1262c5e89e4c9e812fa7f7b08ca1b8df608c
Iryna Walsh <i2pi8bl7@students.codeinstitute.net>
2024-09-21 22:50:12 +0000

    Add:a separate function for clearing selected option and edited fact string  in the array

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,13 +76,6 @@ function getOptions() {
     optionFourBtn.innerText = quizData[3];
 }
 
-
-//-- Engages option selection and triggers an event --//
-function chooseOption() {
-    ;
-}
-
-
 //-- Check correct answer and display the correct one if wrong and activate the facts text --//
 function checkAnswer(event) {
     let selectedOption = questionContent[currentNumber];
@@ -116,15 +109,8 @@ function getFacts() {
     optionFourBtn.classList.remove("hover");
 }
 
-//-- Generate next question when button pressed --//
-function getNextQuestion() {
-    currentNumber += 1;
-    facts.classList.add("hidden-two");
-    factIcon.classList.add("hidden-two");
-    nextQuestionBtn.classList.add("hidden-two");
-    imageNumber();
-    getImage();
-    getOptions();
+//--- Clears the previously selected option and applied functions ---//
+function clearPreviousChoice() {
     optionOneBtn.classList.remove("wrong");
     optionTwoBtn.classList.remove("wrong");
     optionThreeBtn.classList.remove("wrong");
@@ -141,6 +127,17 @@ function getNextQuestion() {
     optionTwoBtn.classList.add("hover");
     optionThreeBtn.classList.add("hover");
     optionFourBtn.classList.add("hover");
+}
+//-- Generate next question when button pressed --//
+function getNextQuestion() {
+    currentNumber += 1;
+    clearPreviousChoice();
+    facts.classList.add("hidden-two");
+    factIcon.classList.add("hidden-two");
+    nextQuestionBtn.classList.add("hidden-two");
+    imageNumber();
+    getImage();
+    getOptions();
 }
 
 //--- Add to the score if answer correct ---//
@@ -195,7 +192,7 @@ const questionContent = [
         number: 5,
         img: "assets/images/henri-matisse.jpg",
         answers: ["Henri Matisse", "Othon Friesz", "Andre Derain", "Georges Braque"],
-        facts: "'Blue Nude II' by Henri Matisse. One of the series of collages, and related color lithographs, made from paper cut-outs depicting nude figures in various positions. Restricted by his physical condition after his surgery for stomach cancer, Matisse began creating art by cutting and painting sheets of paper by hand.",
+        facts: "'Blue Nude II' by Henri Matisse. One of the series of collages, and related color lithographs, made from paper cut-outs depicting nude figures in various positions.",
         correctAnswerIndex: 0
 
     },
@@ -211,14 +208,14 @@ const questionContent = [
         number: 7,
         img: "assets/images/pablo-picasso.jpg",
         answers: ["Paul Cezanne", "Juan Gris", "Pablo Picasso", "Piet Mondrian"],
-        facts: "'Portrait of Dora Mar' is a 1937 oil on canvas painting by Pablo Picasso.In this portrait It depicts Dora Maar, the painter's lover, seated on a chair; her face is particularly remarkable for its experimental style, as the image depicts both the profile of the face and the frontal face in conjunction, which provides an opportunity to convey several perspectives.",
+        facts: "'Portrait of Dora Mar' is a 1937 oil on canvas painting by Pablo Picasso. This portrait is of the painter's lover, seated on a chair; it depicts both the profile and the frontal face in conjunction, which provides an opportunity to convey several perspectives.",
         correctAnswerIndex: 2
     },
     {
         number: 8,
         img: "assets/images/paul-gauguin.jpg",
         answers: ["Vincent Van Gogh", "Paul Gauguin", "Edgar Degas", "Henri Matisse"],
-        facts: "'What's New?' by Paul Gauguin. He was a French painter, sculptor, printmaker, ceramist, and writer, whose work has been primarily associated with the Post-Impressionist and Symbolist movements. This painting is part of series created during his trip to Tahiti, depicting local villegers",
+        facts: "'What's New?' by Paul Gauguin. He was a French artist and a writer, associated with the Post-Impressionist and Symbolist movements. This painting is part of series created during his trip to Tahiti, depicting local villegers",
         correctAnswerIndex: 1
 
     },
@@ -226,7 +223,7 @@ const questionContent = [
         number: 9,
         img: "assets/images/salvador-dali.jpg",
         answers: ["Salvador Dali", "Rene Magritte", "Max Ernst", "Joan Miro"],
-        facts: "'The Persistence of Memory' is a 1931 painting by artist Salvador Dali and one of the most recognizable works of Surrealism.It epitomizes Dali's theory of 'softness' and 'hardness', which was central to his thinking at the time.",
+        facts: "'The Persistence of Memory' is a 1931 painting by artist Salvador Dali and one of the most recognizable works of Surrealism; depicting Dalis phylosophy regarding the consept of 'time'.",
         correctAnswerIndex: 0
 
     },
@@ -234,14 +231,14 @@ const questionContent = [
         number: 10,
         img: "assets/images/vincent-van-gogh.jpg",
         answers: ["Claude Monet", "Paul Gauguin", "Eduard Manet", "Vincent Van Gogh"],
-        facts: "'Sunflowers ' is the title of two series of still life paintings by the Dutch painter Vincent van Gogh. The first series, executed in Paris in 1887, depicts the flowers lying on the ground, while the second set, made a year later in Arles, shows a bouquet of sunflowers in a vase.",
+        facts: "'Sunflowers ' is the title of two series of still life paintings by the Dutch painter Vincent van Gogh. Part of the second series, executed in Paris in 1888, depicts a still-life bouquet of sunflowers in a vase.",
         correctAnswerIndex: 3
     },
     {
         number: 11,
         img: "assets/images/wassily-kandinsky.jpg",
         answers: ["Kazimir Malevich", "El Lissitzky", "Wassily Kandinsky", "Paul Klee"],
-        facts: "'Composition IV' created by Wassily Kandinsky in 1913. It is part of series inspired by music; he theorised that (for example), yellow is the colour of middle C on a brassy trumpet; black is the colour of closure, and the end of things; and that combinations of colours produce vibrational frequencies, akin to chords played on a piano. Kandinsky is generally credited as one of the pioneers of abstraction in western art.",
+        facts: "'Composition IV' created by Wassily Kandinsky in 1913. It is part of series inspired by music; he theorised that the combinations of colours produce vibrational frequencies, akin to chords played on a piano. Kandinsky is generally credited as one of the pioneers of abstraction in western art.",
         correctAnswerIndex: 2
 
     }
