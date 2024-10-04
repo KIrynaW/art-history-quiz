9c3cdb5da51dcc660276a311a952f9d2bef30c54
Iryna Walsh <i2pi8bl7@students.codeinstitute.net>
2024-10-04 11:59:31 +0000

    Script:testing toggle div in getImage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,9 +54,9 @@ function getImage() {
     const quizImage = document.createElement("img");
     quizImage.src = questionContent[currentNumber].img;
     quizImage.alt = questionContent[currentNumber].alt;
-    quizImage.toggleAttribute("img");
     quizImage.id = "art-works";
     imageBox.appendChild(quizImage);
+    quizImage.toggleAttribute("div");
 }
 
 /** 
